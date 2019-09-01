af793ed0f65ae16ad43453b5661e8479a612de66
Angeline Pho <apho8884@gmail.com>
2019-09-01 14:54:27 -0700

    More powershell scripts:

---
+++ hash.ps1
@@ -0,0 +1,3 @@
+﻿Get-ChildItem -path C:\Users\Angeline\Desktop | Get-FileHash
+Get-ChildItem -path C:\Users\Angeline\Desktop | ls | Get-FileHash
+$dir | where { $_.PsIsContainer -eq $false }
---
+++ program.ps1
@@ -0,0 +1,19 @@
+﻿$loc = Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall
+$names = $loc | foreach-object {Get-ItemProperty $_.PsPath}
+foreach ($name in $names)
+{
+    If(-Not [string]::IsNullOrEmpty($name.DisplayName)) {      
+        $line = $name.DisplayName + ',' + $name.DisplayVersion + ',' + $name.InstallDate
+        Write-Host $line   
+    }
+}
+
+$loc = Get-ChildItem HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
+$names = $loc | foreach-object {Get-ItemProperty $_.PsPath}
+foreach ($name in $names)
+{
+    If(-Not [string]::IsNullOrEmpty($name.DisplayName)) {      
+        $line = $name.DisplayName + ','+$name.DisplayVersion + ','+$name.InstallDate
+        Write-Host $line
+    }
+}
